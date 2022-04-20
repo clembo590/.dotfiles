868bc46e26e4103b2f00fd9c25e3dee201dd86b1
clem <clembo590@gmail.com>
2022-04-20 08:21:20 +0200

    change python script to python3

--- scripts/cli.sh
+++ scripts/cli.sh
@@ -1,6 +1,6 @@
 install_python_packages() {
   info "Installing pip"
-  curl https://bootstrap.pypa.io/get-pip.py | python
+  curl https://bootstrap.pypa.io/get-pip.py | python3
 
   info "Installing pip xkcdpass"
   pip install xkcdpass
