66f6634deb99359e5bcd91acf46530aae5ad2cf0
clem <clembo590@gmail.com>
2022-04-21 18:31:46 +0200

    install brew using dump file

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,62 +1,6 @@
-taps=(
-  hashicorp/tap
-  helm/tap
-  homebrew/cask
-  homebrew/cask-fonts
-  homebrew/core
-  vmware-tanzu/carvel
-)
-
-packages=(
-  bat             # styled cat
-  bottom          # tree listing for files
-  broot
-  cmake
-  ctags
-  curl
-  duti            # to set default handlers for file types in MacOS
-  gettext
-  go
-  hashicorp/tap/terraform
-  http-server
-  httpie
-  imagemagick
-  jq
-  kubernetes-cli
-  lazydocker
-  libpq
-  macpass
-  mas
-  minikube
-  node
-  nmap
-  openjdk
-  openssl
-  postgresql
-  pyenv
-  protobuf
-  ripgrep          # fuzzy grep
-  rlwrap
-  rustup
-  sqlite
-  stern
-  stow
-  telnet
-  websocat         # telnet-like for websockets
-  yarn
-  ytt
-  wget
-  zsh
-  zsh-autosuggestions
-  zsh-syntax-highlighting
-)
-
 install_packages() {
-  info "Configuring taps"
-  apply_brew_taps "${taps[@]}"
-
-  info "Installing packages..."
-  install_brew_formulas "${packages[@]}"
+  info "installing brew packages"
+  brew bundle --file Brewfile
 
   info "Cleaning up brew packages..."
   brew cleanup
