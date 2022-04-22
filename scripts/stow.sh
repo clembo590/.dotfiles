777f730f3f39e30ce19a8f57dfbb419eabb1c906
clem <clembo590@gmail.com>
2022-04-22 14:34:17 +0200

    remove files that will be linked

--- scripts/stow.sh
+++ scripts/stow.sh
@@ -1,4 +1,16 @@
 stow_dotfiles() {
+
+  local files=(
+    ".gitconfig"
+    ".zshrc"
+  )
+  
+  info "Removing existing config files"
+  for f in $files; do
+    rm -f "$HOME/$f" || true
+  done
+
+
   local dotfiles="git ssh zsh"
   info "Stowing: $dotfiles"
   stow -d stow --verbose 1 --target $HOME $dotfiles
