f0c408373e5154250166ebe897e8d68ee2b2c8dc
clem <clembo590@gmail.com>
2022-05-25 10:59:08 +0200

    add removeDuplicateEmptyLinesFromModifiedFiles

--- allScripts/removeDuplicateEmptyLines.sh
+++ allScripts/removeDuplicateEmptyLines.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 echo EXECUTED FROM THIS FOLDER $(pwd)
-echo removing double empty lines inside the file $1
-sed -i '' '$!N; /^\(.*\)\n\1$/!P; D' $1
+echo removing double empty lines inside the files $@
+sed -i '' '$!N; /^\(.*\)\n\1$/!P; D' $@
