c8d2346d73f87dd4939504823ff0b71352dcc96c
clem <clembo590@gmail.com>
2022-08-08 09:21:27 +0200

    formatWithGoogle and chromeMusic.sh

---
+++ raycast/chrometidal.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title chrome***REMOVED***
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.description chrome***REMOVED***
+
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --args --profile-directory=Profile\ 7
+
