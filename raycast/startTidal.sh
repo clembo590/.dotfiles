f92efebc6d2929c0c61d3f214fc53c4ef2730645
clem <clembo590@gmail.com>
2022-08-18 09:34:45 +0200

    add omp 'only modifiedProject'

---
+++ raycast/startTidal.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title start***REMOVED***
+# @raycast.mode fullOutput
+
+# Optional parameters:
+source ~/.zshrc
+start***REMOVED***
+
